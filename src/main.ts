15e941e3ffb69f10c621834cba382da71aedaab0
Gabriel Peixoto <bielpcosta@hotmail.com>
2025-04-06 17:12:23 -0300

    feat: ativa o cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const whitelist = configService.get<string>('CORS_WHITELIST')?.split(',') || [];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
